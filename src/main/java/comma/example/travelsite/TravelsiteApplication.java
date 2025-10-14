36365bcca5812a10bf21fa07f16f67b6687d3e37
Chrishawiller <mchrishawiller@gmail.com>
2025-10-14 12:35:33 +0400

    Initial commit: Travel website with redesigned About section

---
+++ src/main/java/comma/example/travelsite/TravelsiteApplication.java
@@ -0,0 +1,13 @@
+package comma.example.travelsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comma/example/travelsite/TravelsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package comma.example.travelsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
